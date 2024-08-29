d18b71b0700aab8d3a55b580e6e57e501e03812f
TriantafyllosZegos <56814423+TriantafyllosZegos@users.noreply.github.com>
2024-08-29 15:38:20 +0300

    Add files via upload

--- reskill-frontend-app/src/pages/SinglePost.jsx
+++ reskill-frontend-app/src/pages/SinglePost.jsx
@@ -16,7 +16,7 @@ function SinglePost() {
         key={data.id}
         className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 p-20 "
       >
-        <div className="flex flex-col col-span-1 text-justify gap-4">
+        <div className="flex flex-col col-span-1 text-justify gap-6 pr-4">
           <h1 className="font-[700] text-[64px] text-black  text-left">
             {capitalizeFirstLetter(data.title)}
           </h1>{" "}
@@ -35,7 +35,7 @@ function SinglePost() {
             <p className="text-left text-black">{data.paragraph3}</p>
           </h2>
         </div>
-        <div className="flex col-span-1 lg:col-span-1 justify-end pl-0 md:pl-16 lg:pl-16 ">
+        <div className="flex col-span-1 lg:col-span-1 justify-end pl-0 md:pl-16 lg:pl-16 pr-1 ">
           {/*IMAGE */}
           <img src={data.photo} alt="Photo" className="rounded-md" />
         </div>
