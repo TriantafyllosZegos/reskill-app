3a44ea4bbdbc4b1b0184959f77f6d2b61b853d5a
TriantafyllosZegos <56814423+TriantafyllosZegos@users.noreply.github.com>
2024-08-30 13:51:34 +0300

    Update Posts.jsx

--- reskill-frontend-app/src/components/Posts.jsx
+++ reskill-frontend-app/src/components/Posts.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const capitalizeFirstLetter = (str) => {
-  if (!str) return str; // Handle empty or null strings
-  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-};
-
 function Posts({ posts }) {
   return (
     <>
@@ -13,7 +8,7 @@ function Posts({ posts }) {
         <h1 className="text-black font-[600] text-[40px] ">
           Related articles or posts
         </h1>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 ">
           {posts.map((item) => (
             <div key={item.id} className="flex flex-col pb-24  ">
               <Link
